511fcdad178af1842fbf37d7a241385c11a386f0
hanuman <hanuman.multiqos@gmail.com>
2024-05-31 19:32:16 +0530

    with image

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
+import Image from 'next/image';
 import React from 'react'
+import second from '../public/slide-img-prepd.webp'
+
 function Home(){
   return (
+    <>
         <h1>Home</h1>
+        <Image src={second} alt="solution slide image" />
+    </>
   );
 }
 export default Home
